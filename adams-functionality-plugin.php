9ab6b95e66d61af4cf267d436685dd22aedd5456
Adam Heckler <aheckler@users.noreply.github.com>
2015-06-26 09:08:54 -0400

    Enable RICG's advanced image compression
    https://github.com/ResponsiveImagesCG/wp-tevko-responsive-images#advanced-image-compression

--- adams-functionality-plugin.php
+++ adams-functionality-plugin.php
@@ -24,3 +24,10 @@ add_filter( 'auto_update_translation', '__return_true' );
 
 // Disable Jetpack module auto-activation
 add_filter( 'jetpack_get_default_modules', '__return_empty_array' );
+
+// Enable RICG's advanced image compression
+// https://github.com/ResponsiveImagesCG/wp-tevko-responsive-images#advanced-image-compression
+function enable_ricg_advanced_image_compression() {
+   add_theme_support( 'advanced-image-compression' );
+}
+add_action( 'after_setup_theme', 'enable_ricg_advanced_image_compression' );
