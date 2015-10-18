cc16a87dd11886108343591b115595cddb8cd236
Adam Heckler <aheckler@users.noreply.github.com>
2015-10-18 09:21:34 -0600

    Register script correctly as well

--- adams-functionality-plugin.php
+++ adams-functionality-plugin.php
@@ -57,8 +57,8 @@ add_filter( 'the_content', 'output_linked_list_of_categories' );
 
 // Fix Jetpack Subscriptions form when output on a page
 // https://github.com/Automattic/jetpack/issues/2854
-wp_register_script( 'akh_fix_jetpack_subs_form_shortcode', plugins_url( 'js/fix_jetpack_subs_form_shortcode.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
-function akh_enqueue_jp_sub_fix_script() {
+function akh_fix_jp_subs_form() {
+	wp_register_script( 'akh_fix_jetpack_subs_form_shortcode', plugins_url( 'js/fix_jetpack_subs_form_shortcode.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_script( 'akh_fix_jetpack_subs_form_shortcode' );
 }
-add_action( 'wp_enqueue_scripts', 'akh_enqueue_jp_sub_fix_script' );
+add_action( 'wp_enqueue_scripts', 'akh_fix_jp_subs_form' );
