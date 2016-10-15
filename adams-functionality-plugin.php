561a1f6b135e48c062cf406a7763624bd15ff292
Adam Heckler <aheckler@users.noreply.github.com>
2016-10-15 09:12:33 -0400

    Remove alteration of Jetpack RP headline
    Plus some other minor changes

--- adams-functionality-plugin.php
+++ adams-functionality-plugin.php
@@ -5,25 +5,18 @@
  * Description:    Custom code for AdamHeckler.com.
  * Version:        1.0
  * Author:         Adam Heckler
- * Author URI:     http://www.adamheckler.com/
+ * Author URI:     https://www.adamheckler.com/
  * License:        GPLv2 or later
  * License URI:    http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 // Exit if this file is called directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit();
+	exit();
 }
 
 // Disable Jetpack module auto-activation
 add_filter( 'jetpack_get_default_modules', '__return_empty_array' );
-
-// Change Jetpack Related Posts headline
-function akh_change_jp_rp_headline( $headline ) {
-	$headline = sprintf( '<h3 class="jp-relatedposts-headline"><em>%s</em></h3>', esc_html__( 'Related Posts', 'jetpack' ) );
-	return $headline;
-}
-add_filter( 'jetpack_relatedposts_filter_headline', 'akh_change_jp_rp_headline' );
 
 // Disable Akismet debug logging
 add_filter( 'akismet_debug_log', '__return_false' );
@@ -40,9 +33,9 @@ add_filter( 'comments_open', 'akh_close_comments_on_attachment_pages', 10, 2 );
 
 // Remove frontend edit links
 function akh_remove_edit_links( $link ) {
-    if ( ! is_admin() ) {
-        return '';
-    }
-    return $link;
-    }
+	if ( ! is_admin() ) {
+		return '';
+	}
+	return $link;
+}
 add_filter( 'get_edit_post_link', 'akh_remove_edit_links' );
